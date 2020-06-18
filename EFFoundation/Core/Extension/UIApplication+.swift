448ad75b11fc9cef9f8fb56d10f6b6a6bbb02b2f
KittenYang <kittenyang@icloud.com>
2020-06-18 11:42:30 +0800

    bugfix: UIApplication get keyWindow

--- EFFoundation/Core/Extension/UIApplication+.swift
+++ EFFoundation/Core/Extension/UIApplication+.swift
@@ -20,25 +20,40 @@ public extension UIApplication {
     
     func keyWindow() -> UIWindow? {
         guard let currentApplication = UIApplication.shared else { return nil }
+        var targetWindow = currentApplication.keyWindow
         if #available(iOS 13.0, tvOS 13.0, *) {
-            let scenes = currentApplication.connectedScenes
-            if let windowScene = scenes.filter({ $0.activationState == .foregroundActive }).first as? UIWindowScene {
-                return windowScene.windows.filter({ $0.isKeyWindow }).first
+            let scenes = currentApplication.connectedScenes.filter({ $0.activationState == .foregroundActive })
+            var findNormalWindow: Bool = false
+            for scene in scenes where !findNormalWindow {
+                if let windowScene = scene as? UIWindowScene {
+                    for keyWindow in windowScene.windows.filter({ $0.isKeyWindow }) where !findNormalWindow {
+                        if keyWindow.windowLevel != .normal {
+                            let windows = currentApplication.windows
+                            for temp in windows where (!findNormalWindow && temp.windowLevel == .normal) {
+                                targetWindow = temp
+                                findNormalWindow = true
+                                break
+                            }
+                        } else {
+                            targetWindow = keyWindow
+                            findNormalWindow = true
+                            break
+                        }
+                    }
+                }
             }
         } else {
-            var window = currentApplication.keyWindow
-            if let keyWindow = window, keyWindow.windowLevel != .normal {
+            if let keyWindow = targetWindow, keyWindow.windowLevel != .normal {
                 let windows = currentApplication.windows
                 for temp in windows {
                     if temp.windowLevel == .normal {
-                        window = temp
+                        targetWindow = temp
                         break
                     }
                 }
             }
-            return window
         }
-        return nil
+        return targetWindow
     }
 }
 #endif
